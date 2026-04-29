fad505582f45274ed72b7630e8a059e557ffaf6b
Tengerleg Enkhtuvshin <tenkhtuvshin@berkeley.com>
2026-04-30 00:46:20 +0200

    initial project setup with Mathlib

---
+++ Ai4math.lean
@@ -0,0 +1,6 @@
+-- This module serves as the root of the `Ai4math` library.
+-- Import modules here that should be built as part of the library.
+import Ai4math.Basic
+import Ai4math.Definitions
+
+
